4c245c91ab00880415856ed6df712f6d040db3a7
阮佳佳 <250069802@qq.com>
2016-07-21 15:50:03 +0800

    ~

--- upload/install/include/version.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('OE_VERSION', 'v1.0');
-define('OE_EDITION', 'Free')
-?>
--- upload/source/conf/mail.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-        return array(
-        'flag'=>'1','smtp'=>'smtp.qq.com','port'=>'25','sendname'=>'测试','sendmail'=>'163@qq.com','sendpassword'=>'123456','tomail'=>'','sendtype'=>'1',
-        );
-        ?>
