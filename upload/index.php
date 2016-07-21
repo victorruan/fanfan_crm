16d3fe9e6445f7d61ef59455ab81c7c54be33b68
阮佳佳 <250069802@qq.com>
2016-07-21 15:58:29 +0800

    ~

--- upload/index.php
+++ upload/index.php
@@ -4,6 +4,4 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 define('APP_PATH','./App/');
-
-require './Core/index.php';
-
+require "./Core/index.php";
