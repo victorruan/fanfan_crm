60adf86bf10ac5397fe949d1a0f048ab3488e725
阮佳佳 <250069802@qq.com>
2016-07-21 16:30:20 +0800

    ~

--- upload/App/Common/Conf/config.php
+++ upload/App/Common/Conf/config.php
@@ -2,7 +2,7 @@
 return array( 
 
 
-    'URL_MODEL'   =>  0,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    'URL_MODEL'   =>  1,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
 	
     'URL_CASE_INSENSITIVE' =>true, //表示URL访问不区分大小写
 	'URL_ROUTER_ON'   => true, //开启路由
